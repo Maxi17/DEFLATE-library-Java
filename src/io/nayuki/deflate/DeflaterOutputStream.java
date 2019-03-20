d10c550cb83d1b041b717540c9a90e2cd5491f64
Maximilian Gama <maxi.gama@yahoo.com>
2019-03-20 19:06:00 +0000

    improved annotations

--- src/io/nayuki/deflate/DeflaterOutputStream.java
+++ src/io/nayuki/deflate/DeflaterOutputStream.java
@@ -15,8 +15,10 @@ import java.io.OutputStream;
 
 import org.checkerframework.checker.index.qual.IndexFor;
 import org.checkerframework.checker.index.qual.IndexOrHigh;
+import org.checkerframework.checker.index.qual.LTEqLengthOf;
 import org.checkerframework.checker.index.qual.LTLengthOf;
 import org.checkerframework.checker.index.qual.NonNegative;
+import org.checkerframework.common.value.qual.IntRange;
 import org.checkerframework.common.value.qual.MinLen;
 
 
@@ -29,8 +31,9 @@ import org.checkerframework.common.value.qual.MinLen;
  */
 public final class DeflaterOutputStream extends FilterOutputStream {
 	
-	private byte @MinLen(6) [] buffer; // Index starts off as 5, so buffer has to be at lest of size 6
-	private @IndexOrHigh("this.buffer") int index; // The index should be within buffer range or equal to its size
+	private byte @MinLen(5) [] buffer; // Index starts off as 5, so buffer has to be at lest of size 6
+	private @IntRange(from = 5) @LTEqLengthOf("this.buffer") int index; // The index should be within buffer range or equal to its size
+	// It starts off as 5 and should never get lower
 	private boolean isFinished;
 
 
@@ -53,15 +56,10 @@ public final class DeflaterOutputStream extends FilterOutputStream {
 			writeBuffer(false);
 		buffer[(@IndexFor("this.buffer") int) index] = (byte) b; // The index can't get here being equal to buffer.length
 		// as the if statement above makes sure to reset it to 5 if it is.
-		index = (@IndexOrHigh("this.buffer") int) (index + 1); // Explained above why this is safe
+		index = (@IntRange(from = 5) @LTEqLengthOf("this.buffer") int) (index + 1); // Explained above why this is safe
 	}
-	
-	@SuppressWarnings("cast") // There are 2 castings in this method:
-	// The first one is redundant, because Math.min takes as arguments two certainly not negative arguments, so the
-	// result can't be negative. We made sure len is non-negative and index is always smaller than length of buffer
-	// The second cast is because of a false positive of the checker. The method System.arrayCopy provided useful
-	// documentation that helped me calculate the correct offsets for b and buffer arrays, such that chunk variable
-	// always stays within correct bounds.
+
+	@SuppressWarnings("cast") // There are three casts in this method. I have explained each one below.
 	public void write(byte[] b, @IndexOrHigh("#1") int off, @IndexOrHigh("#1") int len) throws IOException {
 		// The offset and the length have to be within the bounds of array b, and can be equal to the size of the array
 		// because the exception is thrown only when they exceed this limit. Otherwise, the method works as expected.
@@ -72,16 +70,21 @@ public final class DeflaterOutputStream extends FilterOutputStream {
 		while (len > 0) {
 			if (index == buffer.length)
 				writeBuffer(false);
+			// The first cast is because of a false positive of the checker. The method System.arrayCopy provided useful
+			// documentation that helped me calculate the correct offsets for b and buffer arrays, such that chunk variable
+			// always stays within correct bounds.
+			// The second one is redundant, because Math.min takes as arguments two certainly not negative arguments, so the
+			// result can't be negative. We made sure len is non-negative and index is always smaller than length of buffer
 			@NonNegative @LTLengthOf(value = {"b","buffer"}, offset = {"off - 1", "this.index - 1"}) int chunk =
 					(@NonNegative int) Math.min(len, buffer.length - index);
 			// Below there is a method call for arrayCopy that takes chunk as an argument for length. I used the
 			// documentation of the method to see how to provide a correct chunk so that the method doesn't throw
 			// an exception. I used the offset parameter of @LTLengthOf annotation to make sure chunk is always passed
-			// correctly to the function and, when we add chunk to index below, the index stays within bounds
+			// correctly to the method and, when we add chunk to index below, the index stays within bounds
 			System.arraycopy(b, off, buffer, index, chunk);
 			off += chunk;
 			len -= chunk;
-			index = index + chunk;
+			index = (@IntRange(from = 5) @LTEqLengthOf("this.buffer") int) (index + chunk); // Explained above why this is safe
 		}
 	}
 	
@@ -108,15 +111,14 @@ public final class DeflaterOutputStream extends FilterOutputStream {
 			finish();
 		out.close();
 	}
-	
-	@SuppressWarnings("cast") // Obviously, index is never lower than 5. When we reset it, it becomes 5. We never
-	//subtract anything from it so it gets lower. Therefore, we can safely cast index - 5 to a non-negative integer.
+
+
 	private void writeBuffer(boolean isFinal) throws IOException {
 		if (isFinished)
 			throw new IllegalStateException();
 
 		// Fill in header fields
-		@NonNegative int len = (@NonNegative int) (index - 5); // The length should be non-negative
+		@NonNegative int len = index - 5; // The length should be non-negative
 		int nlen = len ^ 0xFFFF;
 		buffer[0] = (byte)(isFinal ? 0x01 : 0x00);
 		buffer[1] = (byte)(len >>> 0);
